d0f30e1aef867e27192d763301bb2c8b6f15ea34
Deba-98 <mattia.debartolomeo.98@gmail.com>
2024-11-13 21:43:47 +0100

    main aggiustamenti

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { JobDetailsComponent } from '../pages/job-details/job-details.component'
 import { JobListComponent } from '../pages/job-list/job-list.component';
 
 export const appRoutes: Routes = [
-  { path: '**', redirectTo: 'jobs', pathMatch: 'full' },
   { path: '', redirectTo: '/jobs', pathMatch: 'full' },
   { path: 'jobs', component: JobListComponent },
   { path: 'favorites', component: FavoriteListComponent },
